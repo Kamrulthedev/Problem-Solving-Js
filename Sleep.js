f232582730ef33a812c666f3f898363c9b6672cc
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-29 09:55:30 +0600

    created-function-commit

--- Sleep.js
+++ Sleep.js
@@ -3,6 +3,12 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    
+  return new Promise((resolve) => setTimeout(resolve, millis));
 }
 
+
+(async () => {
+    let t = Date.now();
+    await sleep(100);
+    console.log(Date.now() - t); 
+})();
