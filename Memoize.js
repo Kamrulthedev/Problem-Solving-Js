99929930238b2e7089132bcbfce4cc9697f03ed4
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-29 09:22:55 +0600

    write-full-function

--- Memoize.js
+++ Memoize.js
@@ -5,20 +5,28 @@
  * @return {Function}
  */
 function memoize(fn) {
-    const cache =new Map();
+  const cache = new Map();
 
-    return function(...args) {
-        
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
     }
-}
 
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
 
 // Example usage:
 let callCount = 0;
-const memoizedFn = memoize(function (a, b){
-    callCount += 1;
-    return a + b;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
 });
 
-console.log(memoizedFn(4,5));
+console.log(memoizedFn(4, 5));
+console.log(memoizedFn(2, 3));
 console.log(callCount);
