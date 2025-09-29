bb763655c2769742628c12edbb6fab9a5941a719
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-29 08:49:51 +0600

    update-code

--- ReturnLengthofArgumentsPassed.js
+++ ReturnLengthofArgumentsPassed.js
@@ -1,11 +1,16 @@
+// Return Length of Arguments Passed
+
 /**
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
 var argumentsLength = function(...args) {
-    
+    return args.length;
 };
 
 /**
- * argumentsLength(1, 2, 3); // 3
+ * console.log(argumentsLength(1, 2, 3)); // 3
+ * console.log(argumentsLength("a", true, null, [1,2], {x:5})); // 5
  */
+
+console.log(argumentsLength(1, 2, 3));
