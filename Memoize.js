cd605e5bff4255447192f777aedc64a6897d8708
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-29 09:17:51 +0600

    undefined-code-error

--- Memoize.js
+++ Memoize.js
@@ -5,21 +5,20 @@
  * @return {Function}
  */
 function memoize(fn) {
-    let callCount = 0;
     const cache =new Map();
+
     return function(...args) {
         
     }
 }
 
 
-/** 
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1 
- */
+// Example usage:
+let callCount = 0;
+const memoizedFn = memoize(function (a, b){
+    callCount += 1;
+    return a + b;
+});
+
+console.log(memoizedFn(4,5));
+console.log(callCount);
