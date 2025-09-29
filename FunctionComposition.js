7799cf413e50f84b3f58b2a01acd737343073b7d
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-29 08:28:32 +0600

    Example-usage

--- FunctionComposition.js
+++ FunctionComposition.js
@@ -4,7 +4,6 @@
  */
 var compose = function(functions) {
     return function(x) {
-        // Start from x, apply functions from right to left
         for (let i = functions.length - 1; i >= 0; i--) {
             x = functions[i](x);
         }
@@ -12,7 +11,15 @@ var compose = function(functions) {
     }
 };
 
-/**
- * const fn = compose([x => x + 1, x => 2 * x]);
- * console.log(fn(4)); // 9
- */
+
+const fn1 = compose([x => x + 1, x => 2 * x]);
+console.log(fn1(4)); // 9   => (4*2) + 1
+
+const fn2 = compose([]);
+console.log(fn2(5)); // 5   => identity function
+
+const fn3 = compose([x => x - 2, x => x * x, x => x + 3]);
+console.log(fn3(2)); 
+// Step by step: x+3=5 → (5*5)=25 → (25-2)=23
+
+
