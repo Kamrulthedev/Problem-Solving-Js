3cebeb42c410b81e135101b1d0bd5e75e4725d5c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-30 09:07:05 +0600

    new-loop-function-created

--- IntervalCancellation.js
+++ IntervalCancellation.js
@@ -6,24 +6,44 @@
  * @param {number} t
  * @return {Function}
  */
-var cancellable = function(fn, args, t) {
-    fn(...args); //immediately invoke the function
 
+const IntervalLoop = function (fn, args, t) {
+  fn(...args);
 
-    const Interval = setInterval(() => {fn(...args);}, t);
+  const IntervalKeep = setInterval(() => {
+    fn(...args);
+  }, t);
 
-    return function cancelFn(){
-        clearInterval(Interval)
-    }
+  return function cancelFn(){
+    clearInterval(IntervalKeep)
+  };
 };
 
 
-// Example usage: 
-const logMessage = (message) =>{
-    console.log(message);
-};
 
-const cancel = cancellable(logMessage, ["Hello world!"], 1000);
 
-//cencel after 5 seconds 
-setTimeout(() =>{ cancel(); console.log("Interval cancelled"); }, 5000);
+
+// var cancellable = function (fn, args, t) {
+//   fn(...args); //immediately invoke the function
+
+//   const Interval = setInterval(() => {
+//     fn(...args);
+//   }, t);
+
+//   return function cancelFn() {
+//     clearInterval(Interval);
+//   };
+// };
+
+// // Example usage:
+// const logMessage = (message) => {
+//   console.log(message);
+// };
+
+// const cancel = cancellable(logMessage, ["Hello world!"], 1000);
+
+// //cencel after 5 seconds
+// setTimeout(() => {
+//   cancel();
+//   console.log("Interval cancelled");
+// }, 5000);
