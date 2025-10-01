1117c6a95677b62c0e760d90d3c70b58af380182
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-01 15:10:06 +0600

    create-function-commit

--- Water Bottles.js
+++ Water Bottles.js
@@ -5,3 +5,18 @@
  * @param {number} numExchange
  * @return {number}
  */
+
+var numWaterBottles = function (numBottles, numExchange){
+    let totalDrank = 0;
+    let emptyBottles = 0;
+
+    while(numBottles > 0){
+        totalDrank += numBottles;
+        emptyBottles += numBottles;
+        numBottles = Math.floor(emptyBottles / numExchange);
+        emptyBottles = emptyBottles % numExchange;
+    }
+
+    return totalDrank;
+};
+
