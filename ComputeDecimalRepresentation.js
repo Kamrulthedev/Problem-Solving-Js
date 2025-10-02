8150c55981e4481a7728b1b6217ef937f4e9abf8
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-02 15:16:17 +0600

    full write code

--- ComputeDecimalRepresentation.js
+++ ComputeDecimalRepresentation.js
@@ -4,8 +4,18 @@
  * @param {number} n
  * @return {number[]}
  */
-var decimalRepresentation = function(n) {
-    return new Promise((resolve, reject) =>{
-        
-    }) 
+var decimalRepresentation = function (n) {
+  const result = [];
+
+  while (n > 0) {
+    const basae10Component =
+      Math.pow(10, Math.floor(Math.log10(n))) *
+      Math.floor(n / Math.pow(10, Math.floor(Math.log10(n))));
+    result.push(basae10Component);
+    n -= basae10Component;
+  }
+
+  return result;
 };
+
+
