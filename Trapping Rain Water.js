8ea8f1a98a2fb72669b51dfc7d115a5f86ba021c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-03 18:10:20 +0600

    Min-Heap (height, row, col)

--- Trapping Rain Water.js
+++ Trapping Rain Water.js
@@ -12,5 +12,12 @@ var trapRainWater = function(heightMap) {
 
     if(m < 3 || n < 3) return 0; // no space to trap water
 
+    const visited = Array.from({ length: m}, () => Array(n).fill(false));
+
+    // Min-Heap (height, row, col)
+    const minPriorityQueue = require("@datastructures-js/priority-queue").minPriorityQueue;
+    const heap = new minPriorityQueue({priority: x => x[0]});
+
+    
 
 }
