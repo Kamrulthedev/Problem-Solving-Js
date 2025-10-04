8f626177841397ebea4a8f8b5c2b1dd460931203
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-04 09:34:27 +0600

    create a array function

---
+++ ArrayPrototypeLast.js
@@ -0,0 +1,13 @@
+// Arry Prototype Last
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
