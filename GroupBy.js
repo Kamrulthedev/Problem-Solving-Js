b947de95a26ad3416b9c8b8c023e727f85cd707a
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-04 09:51:39 +0600

    return this result

--- GroupBy.js
+++ GroupBy.js
@@ -4,8 +4,17 @@
  * @param {Function} fn
  * @return {Object}
  */
-Array.prototype.groupBy = function(fn) {
-    
+Array.prototype.groupBy = function (fn) {
+  const result = [];
+
+  for (const item of this) {
+    const key = fn(item);
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(item);
+  }
+  return result;
 };
 
 /**
