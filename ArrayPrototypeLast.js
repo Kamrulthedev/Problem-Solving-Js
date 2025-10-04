2d03a049e314905bba13b4877731f1bc6d247cce
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-04 09:40:36 +0600

    example usage commit

--- ArrayPrototypeLast.js
+++ ArrayPrototypeLast.js
@@ -10,7 +10,7 @@ Array.prototype.last = function() {
     return this[this.length - 1];
 };
 
-/**
- * const arr = [1, 2, 3];
- * arr.last(); // 3
- */
+// Example usage:
+ const arr = [1, 2, 3];
+ console.log(arr.last())
+ 
