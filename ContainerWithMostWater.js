6bc77aa926e45e2584bbeadab8d4ad712886c46b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-04 10:12:36 +0600

    create a function and usage a example commit

---
+++ ContainerWithMostWater.js
@@ -0,0 +1,14 @@
+// Container With Most Water
+
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+};
+
+
+// Example usage:
+console.log(maxArea([1, 2, 3, 4, 5]));
