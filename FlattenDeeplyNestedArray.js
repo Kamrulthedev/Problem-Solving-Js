d91f119b7c4d8e4c095d21c64f38c31af554a2ed
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-05 14:31:20 +0600

    create full code

--- FlattenDeeplyNestedArray.js
+++ FlattenDeeplyNestedArray.js
@@ -6,5 +6,18 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    
+  const result = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item) && n > 0) {
+      result.push(...flat(item, n - 1));
+    } else {
+      result.push(item);
+    }
+  }
+
+  //   return result;
+  return result;
 };
+
+
