dcb61f25f602fcb65afe871f16f51ddbcd24f2c8
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-05 14:00:49 +0600

    Create a map from arr1

--- JoinTwoArraysbyID.js
+++ JoinTwoArraysbyID.js
@@ -5,6 +5,11 @@
  * @param {Array} arr2
  * @return {Array}
  */
-var join = function(arr1, arr2) {
-    
+var join = function (arr1, arr2) {
+  const map = {};
+
+  // Step 1: Create a map from arr1
+  for (const item of arr1) {
+    map[ibj.id] = { ...obj };
+  }
 };
