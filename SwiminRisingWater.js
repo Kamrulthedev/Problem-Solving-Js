d97127e43db06e7a1e275d3929b2ae64246ff3bf
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-06 18:33:00 +0600

    visited const create

--- SwiminRisingWater.js
+++ SwiminRisingWater.js
@@ -4,8 +4,11 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var swimInWater = function(grid) {
-    
+var swimInWater = function (grid) {
+  const t = grid.length;
+  const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+  const visited = Array.from({ length: t}, () => Array(t).fill(false));
+  
+  
 
-    
 };
