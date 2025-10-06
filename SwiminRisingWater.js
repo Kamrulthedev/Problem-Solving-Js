48e5c10a629b57542811a8bf80556f94c8292c94
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-06 18:42:17 +0600

    If reached the bottom-right corner, return the time

--- SwiminRisingWater.js
+++ SwiminRisingWater.js
@@ -19,10 +19,14 @@ var swimInWater = function (grid) {
   pq.enqueue([grid[0][0], 0, 0]);
   visited[0][0] = true;
 
-//   Create a While Loop to process the queue
-while(!pq.isEmpty()){
-   const [t, r, c] = pq.dequeue().element;
+  //   Create a While Loop to process the queue
+  while (!pq.isEmpty()) {
+    const [t, r, c] = pq.dequeue().element;
 
-   
-}
+    //    If reached the bottom-right corner, return the time
+    if (r === n - 1 && c === n - 1) return t;
+    for (const [dr, dc] of directions) {
+        
+    }
+  }
 };
