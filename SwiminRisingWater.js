c0d9568aaefa364759684ab2036409c5e83a204a
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-06 18:46:03 +0600

    return -1 if no path found

--- SwiminRisingWater.js
+++ SwiminRisingWater.js
@@ -26,7 +26,15 @@ var swimInWater = function (grid) {
     //    If reached the bottom-right corner, return the time
     if (r === n - 1 && c === n - 1) return t;
     for (const [dr, dc] of directions) {
-        
+      const nr = r + dr,
+        nc = c + dc;
+      if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
+        visited[nr][nc] = true;
+        pq.enqueue([Math.max(t, grid[nr][nc]), nr, nc]);
+      }
     }
   }
+
+  //   return -1 if no path found
+  return -1;
 };
