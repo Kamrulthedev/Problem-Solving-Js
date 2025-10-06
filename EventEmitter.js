0912129476dcbe26249d72d04defe5ab9185ed40
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-06 19:12:47 +0600

    Initialize here to fix the error

--- EventEmitter.js
+++ EventEmitter.js
@@ -6,6 +6,11 @@ class EventEmitter {
    * @param {Function} callback
    * @return {Object}
    */
+
+  constructor() {
+    this.events = {}; // ✅ Initialize here to fix the error
+  }
+
   subscribe(eventName, callback) {
     return {
       unsubscribe: () => {
@@ -37,15 +42,16 @@ class EventEmitter {
    */
   emit(eventName, args = []) {
     if (!this.events[eventName]) return [];
-    return this.events[eventName].map(cb => cb(...args));
+    return this.events[eventName].map((cb) => cb(...args));
   }
 }
 
-
 // Exmpale usage:
 const emitter = new EventEmitter();
-function onClickCallback() { return 99; }
-const sub = emitter.subscribe('onClick', onClickCallback);
-console.log(emitter.emit('onClick')); // [99]
+function onClickCallback() {
+  return 99;
+}
+const sub = emitter.subscribe("onClick", onClickCallback);
+console.log(emitter.emit("onClick")); // [99]
 sub.unsubscribe(); // remove callback
-console.log(emitter.emit('onClick')); // []
+console.log(emitter.emit("onClick")); // []
