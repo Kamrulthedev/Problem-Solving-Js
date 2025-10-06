4bc1590e1706dddb7e73be92dc6a80e1a095c784
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-06 18:54:19 +0600

    use javascript problem solve

--- SwiminRisingWater.js
+++ SwiminRisingWater.js
@@ -4,40 +4,73 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var swimInWater = function (grid) {
-  const t = grid.length;
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-  const visited = Array.from({ length: t }, () => Array(t).fill(false));
+var swimInWater = function(grid) {
+    const n = grid.length;
+    const visited = Array.from({ length: n }, () => Array(n).fill(false));
+    const directions = [[1,0], [-1,0], [0,1], [0,-1]];
 
-  //   Min-Heap implementation
-  const pq = new MinPriorityQueue({ priority: x[0] });
-  pq.enqueue([grid[0][0], 0, 0]);
-  visited[0][0] = true;
-
-  //   Create a While Loop to process the queue
-  while (!pq.isEmpty()) {
-    const [t, r, c] = pq.dequeue().element;
-
-    //    If reached the bottom-right corner, return the time
-    if (r === n - 1 && c === n - 1) return t;
-    for (const [dr, dc] of directions) {
-      const nr = r + dr,
-        nc = c + dc;
-      if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
-        visited[nr][nc] = true;
-        pq.enqueue([Math.max(t, grid[nr][nc]), nr, nc]);
-      }
+    // Min-Heap (priority queue) implementation
+    class MinHeap {
+        constructor() { this.data = []; }
+        push(val) {
+            this.data.push(val);
+            this._bubbleUp(this.data.length - 1);
+        }
+        pop() {
+            if (this.data.length === 1) return this.data.pop();
+            const top = this.data[0];
+            this.data[0] = this.data.pop();
+            this._bubbleDown(0);
+            return top;
+        }
+        _bubbleUp(i) {
+            const data = this.data;
+            while (i > 0) {
+                const parent = Math.floor((i - 1) / 2);
+                if (data[parent][0] <= data[i][0]) break;
+                [data[parent], data[i]] = [data[i], data[parent]];
+                i = parent;
+            }
+        }
+        _bubbleDown(i) {
+            const data = this.data;
+            const n = data.length;
+            while (true) {
+                let left = 2 * i + 1;
+                let right = 2 * i + 2;
+                let smallest = i;
+                if (left < n && data[left][0] < data[smallest][0]) smallest = left;
+                if (right < n && data[right][0] < data[smallest][0]) smallest = right;
+                if (smallest === i) break;
+                [data[i], data[smallest]] = [data[smallest], data[i]];
+                i = smallest;
+            }
+        }
+        isEmpty() { return this.data.length === 0; }
     }
-  }
 
-  //   return -1 if no path found
-  return -1;
+    const heap = new MinHeap();
+    heap.push([grid[0][0], 0, 0]);
+    visited[0][0] = true;
+
+    while (!heap.isEmpty()) {
+        const [t, r, c] = heap.pop();
+        if (r === n - 1 && c === n - 1) return t;
+
+        for (const [dr, dc] of directions) {
+            const nr = r + dr, nc = c + dc;
+            if (nr >= 0 && nc >= 0 && nr < n && nc < n && !visited[nr][nc]) {
+                visited[nr][nc] = true;
+                heap.push([Math.max(t, grid[nr][nc]), nr, nc]);
+            }
+        }
+    }
+
+    return -1;
 };
+
+
+
 
 // Example usage:
 console.log(
