92f612bcd25e027f164ed155c80a26d582affa1d
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-07 21:28:02 +0600

    return this function

--- AvoidFloodinTheCity.js
+++ AvoidFloodinTheCity.js
@@ -4,36 +4,41 @@
  * @param {number[]} rains
  * @return {number[]}
  */
-var avoidFlood = function(rains) {
-    const n = rains.length;
-    const ans = new Array(n).fill(1);
-    const full = new Map();
-    const dryDays = [];
+var avoidFlood = function (rains) {
+  const n = rains.length;
+  const ans = new Array(n).fill(1);
+  const full = new Map();
+  const dryDays = [];
 
-    for(let i = 0; i < n; i++){
-        const lake = rains[1];
+  for (let i = 0; i < n; i++) {
+    const lake = rains[1];
 
-        if(lake === 0){
-            dryDays.push(i);
-        }else{
-            ans[i] = -1;
-        }
-
-        if(full.has(lake)){
-            // lake already full
-            const last = full.get(lake);
-            // Find a dry day after 'last'
-            let dryIndex = -1;
-            
-            for(let j = 0; j < dryDays.length; j++){
-                if(dryDays[j] > last){
-                    dryIndex = dryDays[j];
-                    dryDays.splice(j, 1);
-                    break;
-                }
-            }
-
-            
-        }
+    if (lake === 0) {
+      dryDays.push(i);
+    } else {
+      ans[i] = -1;
     }
+
+    if (full.has(lake)) {
+      // lake already full
+      const last = full.get(lake);
+      // Find a dry day after 'last'
+      let dryIndex = -1;
+
+      for (let j = 0; j < dryDays.length; j++) {
+        if (dryDays[j] > last) {
+          dryIndex = dryDays[j];
+          dryDays.splice(j, 1);
+          break;
+        }
+      }
+
+      if (dryIndex === -1) {
+        return [];
+      }
+      ans[dryIndex] = lake;
+    }
+    full.set(lake, i);
+  }
+  return ans;
 };
