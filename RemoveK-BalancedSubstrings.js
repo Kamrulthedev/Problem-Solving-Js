48b626c7944293c50390a4859cc1f69b4beb0242
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-07 22:27:15 +0600

    Example Walkthrough

--- RemoveK-BalancedSubstrings.js
+++ RemoveK-BalancedSubstrings.js
@@ -4,5 +4,39 @@
  * @return {string}
  */
 var removeSubstring = function(s, k) {
-    
+    const stack = [];
+    for (const ch of s) {
+        stack.push(ch);
+        // check if the top 2k characters form '(' * k + ')' * k
+        if (stack.length >= 2 * k) {
+            const lastPart = stack.slice(-2 * k);
+            if (lastPart.join('') === '('.repeat(k) + ')'.repeat(k)) {
+                // remove them
+                for (let i = 0; i < 2 * k; i++) stack.pop();
+            }
+        }
+    }
+    return stack.join('');
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
