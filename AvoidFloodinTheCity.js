fce27ac8c8a2f473964246b7369714c1adf27fc8
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-07 21:23:51 +0600

    create a for loop

--- AvoidFloodinTheCity.js
+++ AvoidFloodinTheCity.js
@@ -10,5 +10,27 @@ var avoidFlood = function(rains) {
     const full = new Map();
     const dryDays = [];
 
-    
+    for(let i = 0; i < n; i++){
+        const lake = rains[1];
+
+        if(lake === 0){
+            dryDays.push(i);
+        }else{
+            ans[i] = -1;
+        }
+
+        if(full.has(lake)){
+            // lake already full
+            const last = full.get(lake);
+            // Find a dry day after 'last'
+            let dryIndex = -1;
+            
+            for(let j = 0; j < dryDays.length; j++){
+                if(dryDays[j] > last){
+                    dryIndex = dryDays[j];
+                    
+                }
+            }
+        }
+    }
 };
