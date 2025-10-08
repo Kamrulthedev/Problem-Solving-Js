9d8a9f8eae93df4020228213eb7090ed12927c75
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-08 16:58:48 +0600

    create a function successfulPairs

---
+++ SuccessfulPairsofSpells.js
@@ -0,0 +1,11 @@
+// Successful Pairs of Spells 
+
+/**
+ * @param {number[]} spells
+ * @param {number[]} potions
+ * @param {number} success
+ * @return {number[]}
+ */
+var successfulPairs = function(spells, potions, success) {
+    
+};
