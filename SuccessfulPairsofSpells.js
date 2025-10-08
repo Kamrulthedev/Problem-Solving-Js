372f9d59b57d1a72a1d38c930dddf717584eb6c4
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-08 17:06:42 +0600

    this function full created

--- SuccessfulPairsofSpells.js
+++ SuccessfulPairsofSpells.js
@@ -14,6 +14,16 @@ var successfulPairs = function(spells, potions, success) {
     for(let spell of spells){
         let left = 0, rigth = n -1, idx = n;
         
-        
+        while(left <= rigth){
+            let mid = Math.floor((left + rigth) / 2);
+            if(spell * potions[mid] >= success){
+                idx = mid;
+                rigth = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        result.push(n - idx);
     }
+    return result;
 };
