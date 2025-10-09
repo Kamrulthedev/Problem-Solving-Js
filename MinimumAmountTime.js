1eeb4100c8c8139c8e5af07108ac44f2a5fa591b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-09 10:22:10 +0600

    create a function minTime

---
+++ MinimumAmountTime.js
@@ -0,0 +1,10 @@
+// Find the Minimum Amount of Time to Brew Potions
+
+/**
+ * @param {number[]} skill
+ * @param {number[]} mana
+ * @return {number}
+ */
+var minTime = function(skill, mana) {
+    
+};
