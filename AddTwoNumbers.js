437fa587c1f70f3b2c12408a2181dcbb1653473c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-10 11:46:49 +0600

    create a while loop

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -11,5 +11,15 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+
+    while(l1 != null || l2 != null || carry > 0){
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
+        const sum = val1 + val2 + carry;
+
+        
+    }
 };
