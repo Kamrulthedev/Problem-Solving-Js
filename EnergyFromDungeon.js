fef0deb026cf36a0f255fb0c7e8ed58ac3b38147
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-10 11:36:35 +0600

    return maxEnergy

--- EnergyFromDungeon.js
+++ EnergyFromDungeon.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var maximumEnergy = function(energy, k) {
-    
+    const n = energy.length;
+    const dp = new Array(n).fill(0);
+    let maxEnergy = -Infinity;
+
+    // 
+    for(let i = n - 1; i >= 0; i--){
+        if(i + k < n){
+            dp[i] = energy[i] + dp[i + k];
+        }else{
+            dp[i] = energy[i];
+        }
+        maxEnergy = Math.max(maxEnergy, dp[i]);
+    }
+    return maxEnergy;
 };
+
+
+
