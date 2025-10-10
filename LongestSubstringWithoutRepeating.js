6a0ccc6d7782719c197101175c3be7755d1b2934
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-10 12:46:14 +0600

    return maxLen

--- LongestSubstringWithoutRepeating.js
+++ LongestSubstringWithoutRepeating.js
@@ -2,6 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    
+var lengthOfLongestSubstring = function (s) {
+  let set = new Set();
+  let left = 0;
+  let maxLen = 0;
+
+  for (let rigth = 0; rigth < s.length; rigth++) {
+    while (set.has(s[rigth])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[rigth]);
+    maxLen = Math.max(maxLen, rigth - left + 1);
+  }
+  return maxLen;
 };
