bcb64ae71c7ff0f78bd8bceb0f774d5d79a9b73e
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-11 23:55:49 +0600

    Ensure nums1 is the smaller array

--- MedianofTwoSortedArrays.js
+++ MedianofTwoSortedArrays.js
@@ -4,10 +4,11 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+    // Ensure nums1 is the smaller array
     if(nums1.length > nums2.length){
          [nums1, nums2] = [nums2, nums1];
     }
 
 
-    
+
 };
