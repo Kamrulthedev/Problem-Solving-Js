f98b31600b98487d603cb274d055c7950094b9d5
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-12 13:23:24 +0600

    create a function magicalSum

---
+++ FindSumofArrayProduct.js
@@ -0,0 +1,12 @@
+// Find Sum of Array Product of Magical Sequences
+
+
+/**
+ * @param {number} m
+ * @param {number} k
+ * @param {number[]} nums
+ * @return {number}
+ */
+var magicalSum = function(m, k, nums) {
+    
+};
