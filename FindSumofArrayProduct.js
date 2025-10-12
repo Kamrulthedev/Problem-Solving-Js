635bce08d4770b81c1f5fc74910bd2f282e24f3b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-12 13:37:36 +0600

    Sum up results where mask has exactly k bits set

--- FindSumofArrayProduct.js
+++ FindSumofArrayProduct.js
@@ -12,7 +12,29 @@ var magicalSum = function(m, k, nums) {
     const MOD =le9 + 7;
 
     // dp[len][mask] = sum of products
-    const dp = Array.from({length : m + 1}, ()=>{
-        const 
-    })
+    const dp = Array.from({length : m + 1}, ()=> new Map());
+    dp[0].set(0, 1);
+
+    for(let len = 0; len < m; len++){
+        for(const [mask, val] of dp[len]){
+            for(let i =0; i < n; i++){
+                const newMask = mask | (1 << i);
+                const newVal = (val * nums[i]) % MOD;
+                dp[len + 1].set(
+                    newMask,
+                    (dp[len + 1].get(newMask) || 0) + newVal
+                );
+                dp[len + 1].set(newMask, dp[len + 1].get(newMask) % MOD);
+            }
+        }
+    }
+
+    // Sum up results where mask has exactly k bits set
+    let res = 0;
+    for(const [mask, val] of dp[m]){
+        if(countBits (mask) === k){
+            res = (res + val) % MOD;
+        }
+    }
+    return res;
 };
