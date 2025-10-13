3e08c6bdcaf69b52af8153bd64ac965fa324ab2f
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-13 23:35:17 +0600

    create a for loop

--- FindResultantArrayAfterRemoving.js
+++ FindResultantArrayAfterRemoving.js
@@ -3,5 +3,12 @@
  * @return {string[]}
  */
 var removeAnagrams = function(words) {
-    
+    const result = [words[0]];
+
+    for(let i = 1; i< words.length; i++){
+        const prev = result[result.length - 1];
+        const curr = words[i];
+
+        
+    }
 };
