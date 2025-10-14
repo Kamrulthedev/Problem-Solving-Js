ec559ff245dcfb37ef36c8841fc8786f50f5f80b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-14 23:33:22 +0600

    new created code write this function

--- AdjacentIncreasingSubarraysDetection.js
+++ AdjacentIncreasingSubarraysDetection.js
@@ -4,5 +4,20 @@
  * @return {boolean}
  */
 var hasIncreasingSubarrays = function(nums, k) {
-    
+    const n = nums.length;
+
+    const isIncreasing = (start) => {
+        for (let i = start; i < start + k - 1; i++) {
+            if (nums[i] >= nums[i + 1]) return false;
+        }
+        return true;
+    };
+
+    for (let i = 0; i + 2 * k <= n; i++) {
+        if (isIncreasing(i) && isIncreasing(i + k)) {
+            return true;
+        }
+    }
+
+    return false;
 };
