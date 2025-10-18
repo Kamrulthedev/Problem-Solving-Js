bd6a120205235f831e3aa9221fd4010a00741fba
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-18 12:20:11 +0600

    Count the frequency of each remainder when divided by 'value'

--- FindSmallestInteger.js
+++ FindSmallestInteger.js
@@ -4,5 +4,10 @@
  * @return {number}
  */
 var findSmallestInteger = function(nums, value) {
-    
+    const modCount = new Map();
+
+    // Count the frequency of each remainder when divided by 'value'
+    for(const num of nums){
+
+    }
 };
