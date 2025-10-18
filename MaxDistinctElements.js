33c6ad3f2e0c77bde1effa4455059dfd4bd68feb
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-18 12:30:06 +0600

    else cannot place this element distinctively

--- MaxDistinctElements.js
+++ MaxDistinctElements.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
 var maxDistinctElements = function(nums, k) {
-    
+    nums.sort((a,b) => a-b);
+    let count = 0;
+    let prev = -Infinity;
+
+    for (let num of nums) {
+        // Adjust the number to be at least prev+1
+        let adjusted = Math.max(prev + 1, num - k);
+        if (adjusted <= num + k) { // check if adjustment is possible
+            count++;
+            prev = adjusted;
+        }
+        // else cannot place this element distinctively
+    }
+
+    return count;
 };
