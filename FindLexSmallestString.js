626799cf715e0d3f84b272567feddc449752a23b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-19 22:21:32 +0600

    Example usage

--- FindLexSmallestString.js
+++ FindLexSmallestString.js
@@ -4,41 +4,42 @@
  * @param {number} b
  * @return {string}
  */
-var findLexSmallestString = function(s, a, b) {
-    const visited = new Set();
-    let smallest = s;
+var findLexSmallestString = function (s, a, b) {
+  const visited = new Set();
+  let smallest = s;
 
-    // Add Operation
-    const addOperation = (str) =>{
-        const arr = str.split('');
-        for(let i =1; i < arr.length; i +=2){
-            arr[i] = ((+arr[i] + a) % 10).toString();
-        }
-        return arr.join('')
-    };
+  // Add Operation
+  const addOperation = (str) => {
+    const arr = str.split("");
+    for (let i = 1; i < arr.length; i += 2) {
+      arr[i] = ((+arr[i] + a) % 10).toString();
+    }
+    return arr.join("");
+  };
 
-    // Rotate Operation
-    const rotateOperation = (str) => {
-           const n = str.slice(n - b) + str.slice(0, n - b);
-    };
+  // Rotate Operation
+  const rotateOperation = (str) => {
+    const n = str.length;
+    return str.slice(n - b) + str.slice(0, n - b);
+  };
 
-    // Dfs
-    const dfs = (str) =>{
-        if(visited.has(str)) return;
-        visited.add(str);
+  // Dfs
+  const dfs = (str) => {
+    if (visited.has(str)) return;
+    visited.add(str);
 
-        if(str < smallest) smallest = str;
+    if (str < smallest) smallest = str;
 
-        const added = addOperation(str);
-        const rotated = rotateOperation(str);
+    const added = addOperation(str);
+    const rotated = rotateOperation(str);
 
-        dfs(added);
-        dfs(rotated);
-    };
+    dfs(added);
+    dfs(rotated);
+  };
 
-    dfs(s);
-    return smallest;
-
+  dfs(s);
+  return smallest;
 };
 
-
+// Example usage:
+console.log(findLexSmallestString("4353", 9, 4));
