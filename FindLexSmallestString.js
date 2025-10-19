0a8d5b1726e741de35e97b1680b5dccbca05eaf0
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-19 22:14:09 +0600

    Add Operation commit

--- FindLexSmallestString.js
+++ FindLexSmallestString.js
@@ -5,5 +5,17 @@
  * @return {string}
  */
 var findLexSmallestString = function(s, a, b) {
-    
+    const visited = new Set();
+    let smallest = s;
+
+    // Add Operation
+    const addOperation = (str) =>{
+        const arr = str.split('');
+        for(let i =1; i < arr.length; i +=2){
+            arr[i] = ((+arr[i] + a) % 10).toString();
+        }
+        return arr.join('')
+    }
+
+    const 
 };
