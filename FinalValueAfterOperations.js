25a6e663f8798470d3a5d215327dadd9f2365f23
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-20 21:58:05 +0600

    write a for loop

--- FinalValueAfterOperations.js
+++ FinalValueAfterOperations.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    
+    let x = 0;
+
+    for(let op of operations){
+        if(op.includes('+')){
+            x +=1;
+        }else{
+            x -=1;
+        }
+    }
+    return x;
 };
+
+
