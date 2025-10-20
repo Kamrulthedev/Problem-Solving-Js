dfaf64f11c500eccd7a757cbeebca3a23ee2d6a5
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-21 01:14:06 +0600

    32-bit signed integer range check

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    const sign = x < 0 ? -1 : 1;
+    const reversed = parseInt(Math.abs(x).toString().split('').reverse().join('')) * sign;
+
+    // 32-bit signed integer range check 
+    if(reversed < -(2 ** 31) || reversed > (2 ** 31 - 1)){
+        return 0;
+    }
+    return reversed;
 };
+
