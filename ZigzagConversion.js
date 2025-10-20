9bb5ca27919983931c23f70cc4dc1946bd118b0c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-20 22:07:02 +0600

    rows commit

--- ZigzagConversion.js
+++ ZigzagConversion.js
@@ -4,5 +4,11 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
+    if(numRows === 1 || s.length <= numRows) return s;
+
+    const rows = Array.from({length : numRows}, () => "");
+    let currentRow = 0;
+    let goingDown = false;
+
     
 };
