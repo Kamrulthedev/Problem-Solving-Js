3c2e03b5dbd77aab72e3d9ad33e4db9d31053258
Kamrulthedev <kamrulthedevR@gmail.com>
2025-10-20 22:09:52 +0600

    return rows

--- ZigzagConversion.js
+++ ZigzagConversion.js
@@ -10,5 +10,12 @@ var convert = function(s, numRows) {
     let currentRow = 0;
     let goingDown = false;
 
-    
+    for(let char of s){
+        rows[currentRow] += char;
+        if(currentRow === 0 || currentRow === numRows -1){
+            goingDown = !goingDown
+        }
+        currentRow += goingDown ? 1 : -1;
+    }
+    return rows.join('');
 };
