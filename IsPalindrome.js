6316a9bc7963646ce7c8747e048a5d0b7ca8c81c
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-04 19:23:04 +0600

    Negative numbers are never palindrome

--- IsPalindrome.js
+++ IsPalindrome.js
@@ -3,5 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+    // Negative numbers are never palindrome
+    if (x < 0) return false;
+
+    const str = x.toString();
+    const reversed = str.split('').reverse().join('');
+
+    return str === reversed;
 };
