a37f4814e8de2e39af771d52b03c3bd4cfd50096
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-04 19:25:39 +0600

    New Formulation

--- IsPalindrome.js
+++ IsPalindrome.js
@@ -19,3 +19,20 @@ console.log(isPalindrome(121));   // true
 console.log(isPalindrome(-121));  // false
 console.log(isPalindrome(10));    // false
 console.log(isPalindrome(0));     // true
+
+
+
+// New Formulation
+var isPalindrome = function(x) {
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversed = 0;
+    while (x > reversed) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+
+    // For odd digits: remove middle digit using Math.floor(reversed / 10)
+    return x === reversed || x === Math.floor(reversed / 10);
+};
+
