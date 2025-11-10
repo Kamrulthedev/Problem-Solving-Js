837e7385affd1860b2698bd8d0a87d0e7675ad96
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-10 23:49:16 +0600

    we are at start of a non-zero contiguous segment

--- MinOperations.js
+++ MinOperations.js
@@ -3,5 +3,26 @@
  * @return {number}
  */
 var minOperations = function(nums) {
-    
+    let n = nums.length;
+    let i = 0;
+    let ops = 0;
+
+    while (i < n) {
+        // skip zeros
+        if (nums[i] === 0) {
+            i++;
+            continue;
+        }
+
+        // we are at start of a non-zero contiguous segment
+        const seen = new Set();
+        while (i < n && nums[i] !== 0) {
+            seen.add(nums[i]);
+            i++;
+        }
+
+        ops += seen.size;
+    }
+
+    return ops;
 };
