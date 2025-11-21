ae3864e09c2833c6a77803c4da4b1a6e067a6290
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-21 11:21:07 +0600

    Step 1: store first and last occurrence of each character

--- countPalindromicSubsequence.js
+++ countPalindromicSubsequence.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var countPalindromicSubsequence = function(s) {
-    
+  const n = s.length;
+  
+//   Step 1: store first and last occurrence of each character
+const first = Array.apply(26).fill(Infinity);
+const last = Array.apply(26).fill(-1);
+
+
 };
