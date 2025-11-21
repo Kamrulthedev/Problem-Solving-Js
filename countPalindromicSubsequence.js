b33540c1fbd64a45f859a80e6f29b67e24532ca5
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-21 11:23:39 +0600

    write a function on the for loop

--- countPalindromicSubsequence.js
+++ countPalindromicSubsequence.js
@@ -2,12 +2,21 @@
  * @param {string} s
  * @return {number}
  */
-var countPalindromicSubsequence = function(s) {
+var countPalindromicSubsequence = function (s) {
   const n = s.length;
+
+  //   Step 1: store first and last occurrence of each character
+  const first = Array.apply(26).fill(Infinity);
+  const last = Array.apply(26).fill(-1);
+
+  for (let i = 0; i < n; i++) {
+    const idx = s.charCodeAt(i) - 97;
+    first[idx] = Math.min(first[(idx, i)]);
+    last[idx] = Math.max(last[idx], i);
+  }
+
+  let result = 0;
+
+
   
-//   Step 1: store first and last occurrence of each character
-const first = Array.apply(26).fill(Infinity);
-const last = Array.apply(26).fill(-1);
-
-
 };
