53dc70f2daaf73f9580ac26465c18b07c999552f
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-22 13:58:28 +0600

    Example usage

--- minimumOperations.js
+++ minimumOperations.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    
+    const uniqueNums = new Set(nums)
 };
+
+
+
+// Example usage:
+nums = [1, 2, 3, 4, 5 ,6];
+console.log(minimumOperations(nums));
