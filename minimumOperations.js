ab2f1d0cac0757e4156cd1a20244b5271f555f8e
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-22 14:05:54 +0600

    format code

--- minimumOperations.js
+++ minimumOperations.js
@@ -2,21 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minimumOperations = function(nums) {
-    let operations = 0;
+var minimumOperations = function (nums) {
+  let operations = 0;
 
-    for (let num of nums) {
-        let r = num % 3;   
-        if (r !== 0) {
-            operations += 1;  
-        }
+  for (let num of nums) {
+    let r = num % 3;
+    if (r !== 0) {
+      operations += 1;
     }
+  }
 
-    return operations;
+  return operations;
 };
 
-
-
 // Example usage:
-nums = [1, 2, 3, 4, 5 ,6];
+nums = [1, 2, 3, 4, 5, 6];
 console.log(minimumOperations(nums));
