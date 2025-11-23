3c92cfd527a66f5104f0aa7722352dcb8a19bbde
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-23 11:24:06 +0600

    Sort remainder groups (ascending)

--- maxSumDivThree.js
+++ maxSumDivThree.js
@@ -10,9 +10,14 @@ var maxSumDivThree = function (nums) {
   //   Step 1: total sum + categorise remainders
   for (let num of nums) {
     total += num;
-    if(num % 3 === 1) ram1.push(num);
-    else if(num % 3 === 2) ram2.push(num);
+    if (num % 3 === 1) ram1.push(num);
+    else if (num % 3 === 2) ram2.push(num);
   }
+
+  //   Sort remainder groups (ascending)
+  ram1.sort((a, b) => a - b);
+  ram2.sort((a, b) => a - b);
+
 
   
 };
