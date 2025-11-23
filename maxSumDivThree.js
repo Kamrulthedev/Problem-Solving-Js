72b062475868c295c0c32c60dc94ca3f08296c40
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-23 11:21:53 +0600

    for loop created

--- maxSumDivThree.js
+++ maxSumDivThree.js
@@ -2,12 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSumDivThree = function(nums) {
+var maxSumDivThree = function (nums) {
   let total = 0;
   let ram1 = [];
   let ram2 = [];
-  
-  
-//   Step 1: total sum + categorise remainders
 
+  //   Step 1: total sum + categorise remainders
+  for (let num of nums) {
+    total += num;
+    if(num % 3 === 1) ram1.push(num);
+    else if(num % 3 === 2) ram2.push(num);
+  }
+
+  
 };
