6febb43c8c6b0a4c8ca2cd998869dba7b0659cc1
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-24 23:21:12 +0600

    return values only

--- MaxSubsequence.js
+++ MaxSubsequence.js
@@ -4,5 +4,18 @@
  * @return {number[]}
  */
 var maxSubsequence = function(nums, k) {
-    
+    // keep value + index together
+    const arr = nums.map((val, idx) => [val, idx]);
+
+    // sort by value descending
+    arr.sort((a, b) => b[0] - a[0]);
+
+    // pick first k items (largest values)
+    const picked = arr.slice(0, k);
+
+    // now sort picked items by original index
+    picked.sort((a, b) => a[1] - b[1]);
+
+    // return values only
+    return picked.map(item => item[0]);
 };
