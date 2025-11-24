af48884e493e216c52526c3dfdc569b1d0573a78
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-24 23:04:23 +0600

    return answer commit

--- PrefixesDivBy5.js
+++ PrefixesDivBy5.js
@@ -3,5 +3,12 @@
  * @return {boolean[]}
  */
 var prefixesDivBy5 = function(nums) {
-    
+    const answer = [];
+    let mod = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        mod = (mod * 2 + nums[i]) % 5;
+        answer.push(mod === 0);
+    }
+    return answer;
 };
