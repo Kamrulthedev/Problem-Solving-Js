2e4f3b34d68e9554c29593a09af59447236c78de
Kamrulthedev <kamrulthedevR@gmail.com>
2025-11-25 18:46:02 +0600

    full function wirte in the code

--- smallestRepunitDivByK.js
+++ smallestRepunitDivByK.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var smallestRepunitDivByK = function(k) {
-    
+    if(k % 2 === 0 || k % 5 ===0) return -1;
+
+    let mod = 0;
+    for(let length = 1; length <= k; length++){
+        mod = (mod * 10 + 1) % k;
+        if(mod === 0) return length;
+    }
+
+    return -1;
 };
+
+
+
