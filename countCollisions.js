94ac4bdd600f3330d46490b9158532006ae3bce4
Kamrulthedev <kamrulthedevR@gmail.com>
2025-12-04 10:49:39 +0600

    full create this function

--- countCollisions.js
+++ countCollisions.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var countCollisions = function(directions) {
-    
+    let n = directions.length;
+
+    let l = 0;
+    while (l < n && directions[l] === 'L') l++;
+
+    let r = n - 1;
+    while (r >= 0 && directions[r] === 'R') r--;
+
+    let collisions = 0;
+
+    for (let i = l; i <= r; i++) {
+        if (directions[i] !== 'S') collisions++;
+    }
+
+    return collisions;
 };
