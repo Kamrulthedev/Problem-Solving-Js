ecda6abc43c62b23d44ba62a28f6bc7f5703896d
Kamrulthedev <kamrulthedevR@gmail.com>
2025-12-05 15:53:32 +0600

    full function created

--- countPartitions.js
+++ countPartitions.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-var countPartitions = function(nums) {
-    
+var countPartitions = function (nums) {
+    const total = nums.reduce((a, b) => a + b, 0);
+    if (total % 2 !== 0) return 0;
+    return nums.length - 1;
 };
