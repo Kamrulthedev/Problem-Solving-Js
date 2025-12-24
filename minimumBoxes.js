f51a983eb27b8d45c9e1d360243d089d070fdbe7
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-24 23:01:32 +0600

    in case total capacity < total apples

--- minimumBoxes.js
+++ minimumBoxes.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 var minimumBoxes = function(apple, capacity) {
-    
+    let totalApples = apple.reduce((a, b) => a + b, 0);
+    capacity.sort((a, b) => b - a); // largest first
+
+    let count = 0;
+    for (let cap of capacity) {
+        totalApples -= cap;
+        count++;
+        if (totalApples <= 0) return count;
+    }
+
+    return -1; // in case total capacity < total apples
 };
