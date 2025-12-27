6f5aa474e582daf7b84f8fb64f8cc0a062cc62fc
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-27 23:15:01 +0600

    Min heap for available rooms

--- mostBooked.js
+++ mostBooked.js
@@ -4,5 +4,10 @@
  * @return {number}
  */
 var mostBooked = function(n, meetings) {
+    meetings.sort((a, b) => a[0] - b[0]);
+
+    // Min heap for available rooms
+    const available = [];
+    for (let i = 0; i < n; i++) available.push(i);
     
 };
