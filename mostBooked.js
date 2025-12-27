7dbd678e0cb20a8cb96320b64984b29302431c07
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-27 23:45:10 +0600

    Heap helpers

--- mostBooked.js
+++ mostBooked.js
@@ -15,4 +15,16 @@ var mostBooked = function (n, meetings) {
 
       const count = new Array(n).fill(0);
 
+
+      // Heap helpers
+    const heapPush = (heap, item, cmp) => {
+        heap.push(item);
+        let i = heap.length - 1;
+        while (i > 0) {
+            let p = Math.floor((i - 1) / 2);
+            if (cmp(heap[p], heap[i]) <= 0) break;
+            [heap[p], heap[i]] = [heap[i], heap[p]];
+            i = p;
+        }
+    };
 };
