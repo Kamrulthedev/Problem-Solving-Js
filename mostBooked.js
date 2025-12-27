449c5603073f206b540be0ffc7286ed2501836fa
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-27 23:45:53 +0600

    heapPop

--- mostBooked.js
+++ mostBooked.js
@@ -26,4 +26,27 @@ var mostBooked = function (n, meetings) {
       i = p;
     }
   };
+
+const heapPop = (heap, cmp) => {
+        const top = heap[0];
+        const last = heap.pop();
+        if (heap.length > 0) {
+            heap[0] = last;
+            let i = 0;
+            while (true) {
+                let l = i * 2 + 1, r = i * 2 + 2, smallest = i;
+                if (l < heap.length && cmp(heap[l], heap[smallest]) < 0) smallest = l;
+                if (r < heap.length && cmp(heap[r], heap[smallest]) < 0) smallest = r;
+                if (smallest === i) break;
+                [heap[i], heap[smallest]] = [heap[smallest], heap[i]];
+                i = smallest;
+            }
+        }
+        return top;
+    };
+
+
+
+
+
 };
