ea870c152af0ef474dfbee96a307e56f216262a8
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-28 00:07:33 +0600

    main function created

--- subarrayBitwiseORs.js
+++ subarrayBitwiseORs.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var subarrayBitwiseORs = function(arr) {
-    
+    let res = new Set();
+    let cur = new Set();
+
+    for (let num of arr) {
+        let nextCur = new Set([num]);
+        for (let val of cur) {
+            nextCur.add(val | num);
+        }
+        cur = nextCur;
+        for (let val of cur) res.add(val);
+    }
+
+    return res.size;
 };
