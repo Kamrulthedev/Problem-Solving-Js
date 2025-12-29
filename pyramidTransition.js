42cebd20fb53c345ac444ec8da6527ab2dc786bd
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-29 09:48:59 +0600

    code format

--- pyramidTransition.js
+++ pyramidTransition.js
@@ -3,43 +3,42 @@
  * @param {string[]} allowed
  * @return {boolean}
  */
-var pyramidTransition = function(bottom, allowed) {
-    const map = new Map();
+var pyramidTransition = function (bottom, allowed) {
+  const map = new Map();
 
-        // Build mapping
-    for (let str of allowed) {
-        const key = str.slice(0, 2);
-        const val = str[2];
-        if (!map.has(key)) map.set(key, []);
-        map.get(key).push(val);
-    }
+  // Build mapping
+  for (let str of allowed) {
+    const key = str.slice(0, 2);
+    const val = str[2];
+    if (!map.has(key)) map.set(key, []);
+    map.get(key).push(val);
+  }
 
-    //main dfs function 
-function dfs(curr) {
-        if (curr.length === 1) return true;
+  //main dfs function
+  function dfs(curr) {
+    if (curr.length === 1) return true;
 
-        function buildNext(idx, next) {
-            if (idx === curr.length - 1) {
-                return dfs(next);
-            }
+    function buildNext(idx, next) {
+      if (idx === curr.length - 1) {
+        return dfs(next);
+      }
 
-            const key = curr[idx] + curr[idx + 1];
-            if (!map.has(key)) return false;
+      const key = curr[idx] + curr[idx + 1];
+      if (!map.has(key)) return false;
 
-            for (let ch of map.get(key)) {
-                if (buildNext(idx + 1, next + ch)) {
-                    return true;
-                }
-            }
-            return false;
+      for (let ch of map.get(key)) {
+        if (buildNext(idx + 1, next + ch)) {
+          return true;
         }
-
-        return buildNext(0, "");
+      }
+      return false;
     }
 
-    return dfs(bottom);
-};
+    return buildNext(0, "");
+  }
 
+  return dfs(bottom);
+};
 
 // Example usage:
 console.log(pyramidTransition("XYZ", ["XYZ", "YZX"]));
