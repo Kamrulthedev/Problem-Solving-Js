3ad7d1c3287fd9cea5312c49621a27af1bf63b52
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-29 09:46:43 +0600

    Build mapping commit

--- pyramidTransition.js
+++ pyramidTransition.js
@@ -5,5 +5,14 @@
  */
 var pyramidTransition = function(bottom, allowed) {
     const map = new Map();
+
+        // Build mapping
+    for (let str of allowed) {
+        const key = str.slice(0, 2);
+        const val = str[2];
+        if (!map.has(key)) map.set(key, []);
+        map.get(key).push(val);
+    }
+
     
 };
