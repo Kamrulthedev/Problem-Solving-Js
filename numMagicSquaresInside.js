648d302e2e019dee3b2081f7977f00363a4b705d
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2025-12-30 09:40:58 +0600

    update code

--- numMagicSquaresInside.js
+++ numMagicSquaresInside.js
@@ -19,44 +19,5 @@ var numMagicSquaresInside = function(grid) {
             }
         }
 
-        const s =
-            grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
 
-        // Rows
-        for (let i = 0; i < 3; i++) {
-            if (
-                grid[r + i][c] +
-                grid[r + i][c + 1] +
-                grid[r + i][c + 2] !== s
-            ) return false;
-        }
-
-        // Columns
-        for (let j = 0; j < 3; j++) {
-            if (
-                grid[r][c + j] +
-                grid[r + 1][c + j] +
-                grid[r + 2][c + j] !== s
-            ) return false;
-        }
-
-        // Diagonals
-        if (
-            grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] !== s ||
-            grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] !== s
-        ) return false;
-
-        return true;
-    }
-
-    for (let i = 0; i <= rows - 3; i++) {
-        for (let j = 0; j <= cols - 3; j++) {
-            // Center must be 5
-            if (grid[i + 1][j + 1] === 5 && isMagic(i, j)) {
-                count++;
-            }
-        }
-    }
-
-    return count;
 };
