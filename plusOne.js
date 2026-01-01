99aee6b02470f1a3e2c6c7e03b78dbf247703f13
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-01 20:55:47 +0600

    digits

--- plusOne.js
+++ plusOne.js
@@ -3,5 +3,11 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
 };
