860c21e7f83e109eb17f421d27d5a8efbfbbb8d6
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-02 08:37:38 +0600

    Build segment tree commit

--- numOfUnplacedFruits.js
+++ numOfUnplacedFruits.js
@@ -3,6 +3,22 @@
  * @param {number[]} baskets
  * @return {number}
  */
-var numOfUnplacedFruits = function(fruits, baskets) {
+var numOfUnplacedFruits = function (fruits, baskets) {
+    const n = baskets.length;
+    const size = 1 << Math.ceil(Math.log2(n));
+    const seg = new Array(size * 2).fill(0);
+
+    // Build segment tree
+    for (let i = 0; i < n; i++) {
+        seg[size + i] = baskets[i];
+    }
+    for (let i = size - 1; i > 0; i--) {
+        seg[i] = Math.max(seg[i * 2], seg[i * 2 + 1]);
+    }
+
+
     
 };
+
+
+
