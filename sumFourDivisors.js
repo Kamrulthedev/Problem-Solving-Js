2f9b78461839027ab3fb5f5f41f6db90923579ba
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-04 17:02:11 +0600

    Example usage

--- sumFourDivisors.js
+++ sumFourDivisors.js
@@ -2,31 +2,33 @@
  * @param {number[]} nums
  * @return {number}
  */
-var sumFourDivisors = function(nums) {
-    let totalSum = 0;
-    
-        for (let num of nums) {
-        let sum = 0;
-        let count = 0;
-        for (let d = 1; d * d <= num; d++) {
-            if (num % d === 0) {
-                let d2 = num / d;
-                
-                if (d === d2) {
-                    count += 1;
-                    sum += d;
-                } else {
-                    count += 2;
-                    sum += d + d2;
-                }
-                                if (count > 4) break;
-            }
+var sumFourDivisors = function (nums) {
+  let totalSum = 0;
+
+  for (let num of nums) {
+    let sum = 0;
+    let count = 0;
+    for (let d = 1; d * d <= num; d++) {
+      if (num % d === 0) {
+        let d2 = num / d;
+
+        if (d === d2) {
+          count += 1;
+          sum += d;
+        } else {
+          count += 2;
+          sum += d + d2;
         }
-if (count === 4) {
-            totalSum += sum;
-        }
+        if (count > 4) break;
+      }
     }
+    if (count === 4) {
+      totalSum += sum;
+    }
+  }
 
-        return totalSum;
-
+  return totalSum;
 };
+
+// Example usage:
+console.log(sumFourDivisors([21, 3, 4, 5, 6, 7]));
