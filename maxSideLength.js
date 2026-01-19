3c8052db895ae8555f27e953cb7530ae4f70ccb3
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-19 16:10:33 +0600

    format code

--- maxSideLength.js
+++ maxSideLength.js
@@ -10,47 +10,40 @@ var maxSideLength = function (mat, threshold) {
   // Build prefix sum
   const pre = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
 
-
   for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            pre[i][j] =
-                mat[i - 1][j - 1]
-                + pre[i - 1][j]
-                + pre[i][j - 1]
-                - pre[i - 1][j - 1];
+    for (let j = 1; j <= n; j++) {
+      pre[i][j] =
+        mat[i - 1][j - 1] + pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+    }
+  }
+
+  let left = 0;
+  let right = Math.min(m, n);
+  let ans = 0;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    let found = false;
+
+    for (let i = 0; i + mid <= m && !found; i++) {
+      for (let j = 0; j + mid <= n; j++) {
+        const sum =
+          pre[i + mid][j + mid] - pre[i][j + mid] - pre[i + mid][j] + pre[i][j];
+
+        if (sum <= threshold) {
+          found = true;
+          break;
         }
+      }
     }
 
-    let left = 0;
-    let right = Math.min(m, n);
-    let ans = 0;
-
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        let found = false;
-
-        for (let i = 0; i + mid <= m && !found; i++) {
-            for (let j = 0; j + mid <= n; j++) {
-                const sum =
-                    pre[i + mid][j + mid]
-                    - pre[i][j + mid]
-                    - pre[i + mid][j]
-                    + pre[i][j];
-
-                if (sum <= threshold) {
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-         if (found) {
-            ans = mid;
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+    if (found) {
+      ans = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
+  }
 
-    return ans;
+  return ans;
 };
