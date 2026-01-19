305a7cea0319c65b0c1f5e1275361f620856c082
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-19 16:07:30 +0600

    Build prefix sum

--- maxSideLength.js
+++ maxSideLength.js
@@ -3,8 +3,10 @@
  * @param {number} threshold
  * @return {number}
  */
-var maxSideLength = function(mat, threshold) {
-    const m = mat.length;
-    const n = mat[0].length;{
-    
+var maxSideLength = function (mat, threshold) {
+  const m = mat.length;
+  const n = mat[0].length;
+
+  // Build prefix sum
+  const pre = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
 };
