c3ed319f2676f8ac0b0405869eff2a4d104bcec8
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-19 16:07:52 +0600

    for loop created commit

--- maxSideLength.js
+++ maxSideLength.js
@@ -9,4 +9,15 @@ var maxSideLength = function (mat, threshold) {
 
   // Build prefix sum
   const pre = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+
+  for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            pre[i][j] =
+                mat[i - 1][j - 1]
+                + pre[i - 1][j]
+                + pre[i][j - 1]
+                - pre[i - 1][j - 1];
+        }
+    }
 };
