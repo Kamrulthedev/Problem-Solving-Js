33dfb4c95c5f1f591a51c316117eb41e491b5337
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-19 16:08:17 +0600

    while loop created

--- maxSideLength.js
+++ maxSideLength.js
@@ -20,4 +20,27 @@ var maxSideLength = function (mat, threshold) {
                 - pre[i - 1][j - 1];
         }
     }
+
+    let left = 0;
+    let right = Math.min(m, n);
+    let ans = 0;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        let found = false;
+
+        for (let i = 0; i + mid <= m && !found; i++) {
+            for (let j = 0; j + mid <= n; j++) {
+                const sum =
+                    pre[i + mid][j + mid]
+                    - pre[i][j + mid]
+                    - pre[i + mid][j]
+                    + pre[i][j];
+
+                if (sum <= threshold) {
+                    found = true;
+                    break;
+                }
+            }
+        }
 };
