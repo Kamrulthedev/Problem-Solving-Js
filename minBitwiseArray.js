a43da5dbc6bbdca56dc27af39b5953043117657d
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-21 13:42:00 +0600

    Turn off the least significant 1 bit

--- minBitwiseArray.js
+++ minBitwiseArray.js
@@ -4,11 +4,17 @@
  */
 var minBitwiseArray = function(nums) {
     const res = [];
-        for (let n of nums) {
+
+    for (let n of nums) {
         if ((n & (n + 1)) === 0) {
             // n = 2^k - 1 (all ones)
             res.push(-1);
             continue;
         }
+
+        // Find ans[i] = n with trailing zeros preserved
+        let ans = n;
+        // Turn off the least significant 1 bit
+        // so that ans | (ans+1) == n
 
 };
