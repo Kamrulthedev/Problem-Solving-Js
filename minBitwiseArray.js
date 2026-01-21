f9e8145516c6b09bc52af86b1714fbfbee3c0777
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-21 13:41:39 +0600

    Find ans[i] = n with trailing zeros preserved

--- minBitwiseArray.js
+++ minBitwiseArray.js
@@ -4,5 +4,11 @@
  */
 var minBitwiseArray = function(nums) {
     const res = [];
-    
+        for (let n of nums) {
+        if ((n & (n + 1)) === 0) {
+            // n = 2^k - 1 (all ones)
+            res.push(-1);
+            continue;
+        }
+
 };
