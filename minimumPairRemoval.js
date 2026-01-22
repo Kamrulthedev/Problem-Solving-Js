7573133fb048b81dd0ac0069d1f3e17442fb25b3
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-22 15:29:01 +0600

    Replace the pair with their sum

--- minimumPairRemoval.js
+++ minimumPairRemoval.js
@@ -2,43 +2,40 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minimumPairRemoval = function(nums) {
-    
-};/**
+var minimumPairRemoval = function (nums) {}; /**
  * @param {number[]} nums
  * @return {number}
  */
-var minimumPairRemoval = function(nums) {
-    let operations = 0;
+var minimumPairRemoval = function (nums) {
+  let operations = 0;
 
-    const isNonDecreasing = (arr) => {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i - 1]) return false;
-        }
-        return true;
-    };
+  const isNonDecreasing = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) return false;
+    }
+    return true;
+  };
 
-    while (!isNonDecreasing(nums)) {
-        let minSum = Infinity;
-        let idx = 0;
+  while (!isNonDecreasing(nums)) {
+    let minSum = Infinity;
+    let idx = 0;
 
-        // Find adjacent pair with minimum sum
-        for (let i = 0; i < nums.length - 1; i++) {
-            let sum = nums[i] + nums[i + 1];
-            if (sum < minSum) {
-                minSum = sum;
-                idx = i;
-            }
-        }
-
-        // Replace the pair with their sum
-        nums.splice(idx, 2, minSum);
-        operations++;
+    // Find adjacent pair with minimum sum
+    for (let i = 0; i < nums.length - 1; i++) {
+      let sum = nums[i] + nums[i + 1];
+      if (sum < minSum) {
+        minSum = sum;
+        idx = i;
+      }
     }
 
-    return operations;
-};
+    // Replace the pair with their sum
+    nums.splice(idx, 2, minSum);
+    operations++;
+  }
 
+  return operations;
+};
 
 // Example usage:
 console.log(minimumPairRemoval([5, 3, 4, 5, 6]));
