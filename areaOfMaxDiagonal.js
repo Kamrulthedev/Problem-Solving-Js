9a37c159535d2590d024ef289c1b9942f04803e5
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-22 15:32:17 +0600

    full function created

--- areaOfMaxDiagonal.js
+++ areaOfMaxDiagonal.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var areaOfMaxDiagonal = function(dimensions) {
-    
+    let maxDiagonalSq = 0;
+    let maxArea = 0;
+
+    for (let [l, w] of dimensions) {
+        let diagSq = l * l + w * w;
+        let area = l * w;
+
+        if (diagSq > maxDiagonalSq) {
+            maxDiagonalSq = diagSq;
+            maxArea = area;
+        } else if (diagSq === maxDiagonalSq) {
+            maxArea = Math.max(maxArea, area);
+        }
+    }
+
+    return maxArea;
 };
