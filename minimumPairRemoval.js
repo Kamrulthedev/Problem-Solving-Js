5e45e732526eb2cca44c8e4b248cfbdd4c0c3e78
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-22 15:27:19 +0600

    Find adjacent pair with minimum sum

--- minimumPairRemoval.js
+++ minimumPairRemoval.js
@@ -4,4 +4,37 @@
  */
 var minimumPairRemoval = function(nums) {
     
+};/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumPairRemoval = function(nums) {
+    let operations = 0;
+
+    const isNonDecreasing = (arr) => {
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) return false;
+        }
+        return true;
+    };
+
+    while (!isNonDecreasing(nums)) {
+        let minSum = Infinity;
+        let idx = 0;
+
+        // Find adjacent pair with minimum sum
+        for (let i = 0; i < nums.length - 1; i++) {
+            let sum = nums[i] + nums[i + 1];
+            if (sum < minSum) {
+                minSum = sum;
+                idx = i;
+            }
+        }
+
+        // Replace the pair with their sum
+        nums.splice(idx, 2, minSum);
+        operations++;
+    }
+
+    return operations;
 };
