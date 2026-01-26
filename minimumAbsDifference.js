e38021450d8fd8367b956909d54fc308b4963b51
KAMRUL HASSAN <139159388+Kamrulthedev@users.noreply.github.com>
2026-01-26 12:31:16 +0600

    infinity loop created commit

--- minimumAbsDifference.js
+++ minimumAbsDifference.js
@@ -2,7 +2,9 @@
  * @param {number[]} arr
  * @return {number[][]}
  */
-var minimumAbsDifference = function(arr) {
-    arr.sort((a, b) => a - b);  // Step 1: sort array
-    
+var minimumAbsDifference = function (arr) {
+  arr.sort((a, b) => a - b); // Step 1: sort array
+
+  let minDiff = Infinity;
+  let res = [];
 };
